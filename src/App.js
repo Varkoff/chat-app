0e17fc84eeb1154d5fab7e9d0ba90993d0b025db
Virgile.R <virgile.r@itdm-group.com>
2020-11-12 15:33:16 +0100

    React  2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import Contact from "./components/Contact";
 function App() {
 	return (
 		<div>
-			<Contact />
+			<Contact
+				avatar="https://randomuser.me/api/portraits/women/39.jpg"
+				status={true}
+				name="Addison Hart"
+				statusText="online"
+			/>
+			<Contact
+				avatar="https://randomuser.me/api/portraits/women/90.jpg"
+				status={false}
+				name="Amelia Myers"
+				statusText="offline"
+			/>
+			<Contact
+				avatar="https://randomuser.me/api/portraits/men/28.jpg"
+				status={false}
+				name="Albert Collins"
+				statusText="online"
+			/>
 		</div>
 	);
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,35 @@
 import "./Contact.css";
+import PropTypes from "prop-types";
 
-function Contact() {
+function Contact(props) {
 	return (
 		<div className="Contact">
-			<img
-				className="avatar"
-				src="https://randomuser.me/api/portraits/women/39.jpg"
-			/>
+			<img className="avatar" src={props.avatar} />
 			<div>
-				<p className="name">Addison Hart</p>
+				<p className="name">{props.name}</p>
 				<div className="status">
-					<div className="status-online"></div>
-					<p className="status-text">Connecté</p>
+					{props.status ? (
+						<div>
+							<div className="status-online"></div>
+							<p className="status-text">{props.statusText}</p>
+						</div>
+					) : (
+						<div>
+							<div className="status-offline"></div>
+							<p className="status-text">{props.statusText}</p>
+						</div>
+					)}
 				</div>
 			</div>
 		</div>
 	);
 }
+
+Contact.propTypes = {
+	avatar: PropTypes.string.isRequired,
+	status: PropTypes.bool.isRequired,
+	name: PropTypes.string.isRequired,
+	statusText: PropTypes.string.isRequired,
+};
 
 export default Contact;
